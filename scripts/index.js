06513492332f87b2001e297fdcb90621a90d9843
Anna Shlyapkova <annashlyapkova@yandex.ru>
2021-06-21 19:20:28 +0300

    2 review

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,11 @@
 const popupElement = document.querySelector('.popup')
 const popupCloseElement = popupElement.querySelector('.popup__close')
 const popupOpenElement = document.querySelector('.profile__info-button')
-console.log(popupOpenElement, popupCloseElement, popupElement);
-
 const formElement = document.querySelector('.popup__form')
-const formNameElememt = document.querySelector('.popup__item_name')
-const formJobElement = document.querySelector('.popup__item_job')
+const formNameElememt = document.querySelector('.popup__item_type_name')
+const formJobElement = document.querySelector('.popup__item_type_job')
 const profileNameElement = document.querySelector('.profile__name')
 const profileJobElement = document.querySelector('.profile__description')
-
-console.log(formJobElement, formNameElememt, profileJobElement, profileNameElement);
 
 const openPopup = function () {
     formNameElememt.value = profileNameElement.textContent;
