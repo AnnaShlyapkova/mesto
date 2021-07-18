49cd55d96159ea4ac62a87981578aa49788a28d0
Anna Shlyapkova <annashlyapkova@yandex.ru>
2021-07-18 12:53:43 +0300

    changed config

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,37 +1,37 @@
 //Проверка input
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, config) => {
 const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 errorElement.textContent = errorMessage;
-inputElement.classList.add('popup__item-error_active');
+inputElement.classList.add(config.inputErrorClassActive);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, config) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   errorElement.textContent = "";
-  inputElement.classList.remove('popup__item-error_active');
+  inputElement.classList.remove(config.inputErrorClassActive);
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, config) => {
     const isInputNotValid = !inputElement.validity.valid;
   if (isInputNotValid) {
       const errorMessage = inputElement.validationMessage;
-      showInputError(formElement, inputElement, errorMessage);
+      showInputError(formElement, inputElement, errorMessage, config);
   } else {
-      hideInputError(formElement, inputElement);
+      hideInputError(formElement, inputElement, config);
   }
 };
 
 //Активация кнопки
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement, config) => {
   const hasNotValidInput = inputList.some(
     (inputElement) => !inputElement.validity.valid);
   
   if (hasNotValidInput) {
       buttonElement.setAttribute('disabled', true);
-      buttonElement.classList.add('popup__submit-button_disabled');
+      buttonElement.classList.add(config.inactiveButtonClass);
   } else {
     buttonElement.removeAttribute('disabled');
-    buttonElement.classList.remove('popup__submit-button_disabled');
+    buttonElement.classList.remove(config.inactiveButtonClass);
   }
 }
 //Установка слушателей на input
@@ -39,27 +39,29 @@ const setEventListeners = (formElement,config) => {
     formElement.addEventListener('submit', (evt) => {
         evt.preventDefault();
 });
-const inputList = Array.from(formElement.querySelectorAll('.popup__item'));
-const buttonElement = formElement.querySelector('.popup__submit-button');
+const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+const buttonElement = formElement.querySelector(config.submitButtonSelector);
 inputList.forEach(inputElement => {
     inputElement.addEventListener('input', (event) => {
-        checkInputValidity(formElement, inputElement);
-        toggleButtonState(inputList, buttonElement);
+        checkInputValidity(formElement, inputElement, config);
+        toggleButtonState(inputList, buttonElement, config);
     })
 })
 };
 
+
 const enableValidation = (config) => { 
     const formList = Array.from(document.querySelectorAll(config.formSelector)); 
-    const setEventListenersWithConfig = (formElement) => { setEventListeners(formElement,config) }
+    const setEventListenersWithConfig = (formElement) => { 
+      setEventListeners(formElement,config) }
     formList.forEach(setEventListenersWithConfig); 
   }; 
-
+ 
 
 enableValidation({
     formSelector: '.popup__form', 
     inputSelector: '.popup__item', 
     submitButtonSelector: '.popup__submit-button', 
     inactiveButtonClass: '.popup__submit-button_disabled', 
-    inputErrorClass: '.popup__input-error',
+    inputErrorClassActive: '.popup__item-error_active',
 }); 
